e27a5bc2538e7e18da2607d4d6f369437e7ffa13
Shahanashri <shahanashri1552@gmail.com>
2023-03-22 11:50:01 +0530

    Store for cart

--- src/components/OrderButton.svelte
+++ src/components/OrderButton.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
+	import { cart } from './stores';
+
 	export let id: number;
 
-	let count = 0;
+	let count: number;
+	$: count = $cart[id] == undefined ? 0 : $cart[id];
 
-    const incrementCount = () => {
-        count++;
+	const increment = () => {
+		cart.update((foods) => {
+			foods[id] != undefined ? foods[id]++ : (foods[id] = 1);
+			return foods;
+		});
+	};
+
+	const decrement = () => {
+		cart.update((foods) => {
+			if (foods[id] != undefined && foods[id] > 0) foods[id]--;
+            if (foods[id] != undefined && foods[id] === 0) delete foods[id];
+			return foods;
+		});
     };
 </script>
 
 <div>
 	{#if count > 0}
-		<button>-</button> {count} <button>+</button>
+		<div class="incdec">
+			<button on:click={decrement}>-</button><span>{count}</span><button on:click={increment}
+				>+</button
+			>
+		</div>
 	{:else}
-		<button on:click={incrementCount}>Add {id}</button>
+		<button on:click={increment}>Add {id}</button>
 	{/if}
 </div>
 
@@ -27,5 +45,15 @@
 
 	button:active {
 		background: rgba(56, 20, 85, 0.856);
+	}
+
+	.incdec {
+		background: rgb(238, 127, 79);
+		color: aliceblue;
+		border-radius: 0.25rem;
+	}
+
+	span {
+		padding: 0 0.5rem;
 	}
 </style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,7 +1,17 @@
-<script>
+<script lang="ts">
 	import { fade } from 'svelte/transition';
+	import { cart } from '../../components/stores';
+	import { foods } from '../../foods';
+
+	const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
+	let order_ids: number[];
+
+	$: order_ids = getKeys($cart);
 </script>
 
 <div in:fade={{ duration: 500, delay: 500 }} out:fade={{ duration: 500 }}>
 	<h1>Cart</h1>
+	{#each order_ids as order_id}
+		<h3>Food: {foods[order_id].name}, Count: {$cart[order_id]}</h3>
+	{/each}
 </div>
