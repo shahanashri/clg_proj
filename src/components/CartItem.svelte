69f620cad05b50479be3a82e33c477ffb6b2667d
Shahanashri <shahanashri1552@gmail.com>
2023-03-24 22:21:52 +0530

    Cart modified

---
+++ src/components/CartItem.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { foods } from '../foods';
+	import OrderButton from './OrderButton.svelte';
+	import { cart } from './stores';
+
+	export let order_id: number;
+</script>
+
+<div class="cart-item">
+	<h3>{foods[order_id].name}</h3>
+	<div class="order-details">
+		<OrderButton id={order_id} /> <span>x Rs.{foods[order_id].price} =</span>
+		<div class="total">Rs.{foods[order_id].price * $cart[order_id]}</div>
+	</div>
+</div>
+
+<style>
+	.cart-item {
+		padding: 0.7rem;
+		width: 70%;
+		margin: auto;
+		margin-bottom: 1rem;
+		border: 0.1rem solid black;
+		border-radius: 1rem;
+		background: white;
+
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.order-details {
+		display: flex;
+        align-items: center;
+	}
+
+	span {
+		margin: 0 0.5rem 0 0.5rem;
+		font-weight: bold;
+	}
+
+    .total {
+		background: rgb(238, 127, 79);
+        padding: 0.1rem 0.25rem;
+		border: 0.1rem solid black;
+		border-radius: 0.2rem;
+    }
+</style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,17 +1,49 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
+	import CartItem from '../../components/CartItem.svelte';
 	import { cart } from '../../components/stores';
 	import { foods } from '../../foods';
 
 	const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
 	let order_ids: number[];
+	let total: number;
 
 	$: order_ids = getKeys($cart);
+	$: {
+		total = 0;
+		order_ids.forEach((order_id) => {
+			total += $cart[order_id] * foods[order_id].price;
+		});
+	}
 </script>
 
 <div in:fade={{ duration: 500, delay: 500 }} out:fade={{ duration: 500 }}>
 	<h1>Cart</h1>
-	{#each order_ids as order_id}
-		<h3>Food: {foods[order_id].name}, Count: {$cart[order_id]}, Price: {foods[order_id].price * $cart[order_id]}</h3>
+	{#each order_ids as order_id (order_id)}
+		<CartItem {order_id} />
 	{/each}
+	<h3>
+		Total: <span class="total">Rs.{total}</span>
+	</h3>
 </div>
+
+<style>
+	h3 {
+		padding: 0.7rem;
+		width: 70%;
+		margin: auto;
+		margin-bottom: 1rem;
+		border: 0.1rem solid black;
+		border-radius: 1rem;
+		background: white;
+		text-align: end;
+	}
+
+	.total {
+		margin-left: 0.2rem;
+		background: rgb(238, 127, 79);
+		padding: 0.1rem 0.25rem;
+		border: 0.1rem solid black;
+		border-radius: 0.2rem;
+	}
+</style>
