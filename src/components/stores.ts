62725d632408dabd12a79bc71576af6135004c80
Shahanashri <shahanashri1552@gmail.com>
2023-03-24 22:55:53 +0530

    Cart localstorage

--- src/components/stores.ts
+++ src/components/stores.ts
@@ -1,22 +1,33 @@
+import { browser } from "$app/environment";
 import { writable, type Writable } from "svelte/store";
 
+const getFromStorage = () => {
+    if (browser) return JSON.parse(localStorage.getItem('cart') || '{}');
+    return {};
+}
+
+if (browser) {
+    onstorage = () => {
+        cart.set(getFromStorage());
+    }
+}
 
 const createCart = () => {
-    const {subscribe, set, update}: Writable<{[id: number]: number}> = writable({});
+    const { subscribe, set, update }: Writable<{ [id: number]: number }> = writable(getFromStorage());
 
-	const increment = (id: number) => {
-		update((orders) => {
-			orders[id] != undefined ? orders[id]++ : (orders[id] = 1);
-			return orders;
-		});
-	};
+    const increment = (id: number) => {
+        update((orders) => {
+            orders[id] != undefined ? orders[id]++ : (orders[id] = 1);
+            return orders;
+        });
+    };
 
-	const decrement = (id: number) => {
-		update((orders) => {
-			if (orders[id] != undefined && orders[id] > 0) orders[id]--;
+    const decrement = (id: number) => {
+        update((orders) => {
+            if (orders[id] != undefined && orders[id] > 0) orders[id]--;
             if (orders[id] != undefined && orders[id] === 0) delete orders[id];
-			return orders;
-		});
+            return orders;
+        });
     };
 
     const reset = () => {
@@ -27,8 +38,13 @@ const createCart = () => {
         subscribe,
         increment,
         decrement,
-        reset
+        reset,
+        set
     }
 }
 
 export const cart = createCart();
+
+cart.subscribe(value => {
+    if (browser) localStorage.setItem('cart', JSON.stringify(value));
+});
